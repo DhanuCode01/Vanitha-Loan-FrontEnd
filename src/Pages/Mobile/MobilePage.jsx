921d618ded5b438ec9d815d403a1b577efa1bf15
DhanuCode01 <lakshanshanaka327@gmail.com>
2025-08-19 23:51:16 +0530

    Mobile page (SideBar Update) test 3.1

--- src/Pages/Mobile/MobilePage.jsx
+++ src/Pages/Mobile/MobilePage.jsx
@@ -4,6 +4,7 @@ import AccountPage from "../Account/AccountPage";
 import RightBar from "../../Components/RightBar";
 import MobileSideBars from "../../Components/MobileSideBars";
 import SideBarMobile from "../../test/TestSix";
+import AccountPageMobile from "../Account/AccountPageMobile";
 
 
 
@@ -11,13 +12,13 @@ export default function MobilePage() {
   return (
     
     <div className="flex flex-col h-screen overflow-hidden">
-        <div className="w-full max-w-[767px] mx-auto h-[500px] p-3 ">
+        <div className="w-full max-w-[767px] mx-auto h-[500px] p-2 ">
         <SideBarMobile/>
         </div>
         <div className="flex-1 overflow-auto">
         <Routes>
           <Route path="/" element={<CustomerPage/>} />
-          <Route path="/account/:key" element={<AccountPage/>} />
+          <Route path="/account/:key" element={<AccountPageMobile/>} />
         </Routes>
       </div>
     </div>

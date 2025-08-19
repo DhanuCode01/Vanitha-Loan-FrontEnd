bccd038adcc14dc5316f0107891dc333709585a1
DhanuCode01 <lakshanshanaka327@gmail.com>
2025-08-19 23:07:09 +0530

    Mobile page (SideBar Update) V 0.3

--- src/Pages/Mobile/MobilePage.jsx
+++ src/Pages/Mobile/MobilePage.jsx
@@ -3,12 +3,30 @@ import CustomerPage from "../Customer/CustomerPage";
 import AccountPage from "../Account/AccountPage";
 import RightBar from "../../Components/RightBar";
 import MobileSideBars from "../../Components/MobileSideBars";
+import SideBarMobile from "../../test/TestSix";
 
 
 
 export default function MobilePage() {
   return (
-    <div className="flex flex-col   h-screen overflow-hidden">
+    
+    <div className="flex flex-col h-screen overflow-hidden">
+        <div className="w-full max-w-[767px] mx-auto h-[500px] p-3 ">
+        <SideBarMobile/>
+        </div>
+        <div className="flex-1 overflow-auto">
+        <Routes>
+          <Route path="/" element={<CustomerPage/>} />
+          <Route path="/account/:key" element={<AccountPage/>} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+
+
+{/* <div className="flex flex-col   h-screen overflow-hidden">
        <div className="md:w-[100px] w-full">
         <MobileSideBars />
       </div>
@@ -18,6 +36,4 @@ export default function MobilePage() {
           <Route path="/account/:key" element={<AccountPage/>} />
         </Routes>
       </div>
-    </div>
-  );
-}
+    </div> */}
